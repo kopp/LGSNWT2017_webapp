7ec679a1e77886315cc64e65174dc6ce5ee8ace1
Michael Kopp <michael.kopp@lichtenstern.info>
2020-05-14 13:13:56 +0000

    minimal api in flask to add/sub from a number

--- flask_app.py
+++ flask_app.py
@@ -9,4 +9,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "hallo"
+    return "hallo " + adee()
+
+@app.route('/adee')
+def adee():
+    return "adee"
+
+
+n = 1
+
+@app.route('/add')
+def add():
+    global n
+    n = n + 1
+    return str(n)
+
+@app.route('/sub')
+def sub():
+    global n
+    n = n - 1
+    return str(n)
+
+@app.route('/get')
+def get():
+    return str(n)
